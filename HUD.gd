d0602056a3c03b55617c4ee169255deaa3f4d11e
cassiano <honoriocassiano@gmail.com>
2020-08-22 18:25:47 -0300

    Add End Game scene

--- HUD.gd
+++ HUD.gd
@@ -2,15 +2,51 @@ extends CanvasLayer
 
 signal start_game()
 
+const messagesByMinPoints = [
+	[50, "The army is proud of you!"],
+	[0, "You failed the nation!"]
+]
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$StartScreen.show()
+	$EndScreen.hide()
+
+
+func _get_message(points):
 	
-	# Remove this line when implement "Start"
-#	$StartScreen.hide()
-	
-	pass # Replace with function body.
+	for messageByMinPoints in messagesByMinPoints:
+		if points > messageByMinPoints[0]:
+			return messageByMinPoints[1]
+
+
+func _end_game(points):
+	$StartScreen.hide()
+	$EndScreen.show()
+
+#	Set messages
+	$EndScreen/Label.set_text(_get_message(points))
+	$EndScreen/Label2.set_text("Points: " + str(points))
+
+
+func _start_screen():
+	$StartScreen.show()
+	$EndScreen.hide()
+
+
+func _start_game():
+	$StartScreen.hide()
+	$EndScreen.hide()
+
 
 func _on_Button_pressed():
-	$StartScreen.hide()
+	_start_game()
+	
+	emit_signal("start_game")
 
+
+func _on_Button2_pressed():
+	_start_game()
+	
 	emit_signal("start_game")
