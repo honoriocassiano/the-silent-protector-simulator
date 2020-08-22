00752287ffa337c49a43ea9848689f4abf59e635
cassiano <honoriocassiano@gmail.com>
2020-08-22 16:19:47 -0300

    Hide HUD on game start

--- HUD.gd
+++ HUD.gd
@@ -2,6 +2,10 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	# Remove this line when implement "Start"
+	$StartScreen.hide()
+	
 	pass # Replace with function body.
 
 func _on_Button_pressed():
