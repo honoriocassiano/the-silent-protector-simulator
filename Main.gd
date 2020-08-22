73d6da10a11137941e3afb5a6bf57c81187e58cb
cassiano <honoriocassiano@gmail.com>
2020-08-22 11:17:31 -0300

    Fix shoot spawn direction and rotation

--- Main.gd
+++ Main.gd
@@ -9,11 +9,11 @@ export (PackedScene) var Shoot
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
+
 	$ShootSpawnTimer.start()
-	
+
 	randomize()
-	
+
 	pass # Replace with function body.
 
 
@@ -22,21 +22,40 @@ func _ready():
 #	pass
 
 func _on_ShootSpawnTimer_timeout():
-	
+
 	$ShootPath/ShootSpawnLocation.offset = randi()
-	
+
 	var shoot = Shoot.instance()
+
+	var spawnPosition = $ShootPath/ShootSpawnLocation.position
+	
+	var bedPosition = $Bed.position
+	var distance = bedPosition.distance_to(spawnPosition)
+
+#	var cosine = (spawnPosition.x - bedPosition.x) / distance
+#	var sine = (spawnPosition.y - bedPosition.y) / distance
+
+	var originalDirection = $ShootPath/ShootSpawnLocation.rotation
+
+	var direction = -atan2(spawnPosition.x - bedPosition.x, spawnPosition.y - bedPosition.y)
+
+#	position.x = cosine * DISTANCE_FROM_BED + bedPosition.x
+#	position.y = sine * DISTANCE_FROM_BED + bedPosition.y
+
+#	var direction = $ShootPath/ShootSpawnLocation.rotation + PI / 2
+#	var direction = $ShootPath/ShootSpawnLocation.rotation
+	# Set the mob's position to a random location.
+#	shoot.position = $ShootPath/ShootSpawnLocation.position
+	# Add some randomness to the direction.
+#	direction += rand_range(-PI / 4, PI / 4)
+
+#	var finalDirection = originalDirection
+	var finalDirection = direction
+
+	shoot.position = spawnPosition
+	shoot.rotation = finalDirection
+	shoot.linear_velocity = -Vector2(0, 150).rotated(finalDirection)
+#	shoot.linear_velocity = Vector2(rand_range(150, 250), 0).rotated(direction)
+#	shoot.linear_velocity = shoot.linear_velocity.rotated(direction)
 	
 	add_child(shoot)
-	
-	var direction = $ShootPath/ShootSpawnLocation.rotation + PI / 2
-	# Set the mob's position to a random location.
-	shoot.position = $ShootPath/ShootSpawnLocation.position
-	# Add some randomness to the direction.
-	direction += rand_range(-PI / 4, PI / 4)
-	shoot.rotation = direction
-	# Set the velocity (speed & direction).
-	shoot.linear_velocity = Vector2(rand_range(150, 250), 0)
-	shoot.linear_velocity = shoot.linear_velocity.rotated(direction)
-	
-	pass # Replace with function body.
